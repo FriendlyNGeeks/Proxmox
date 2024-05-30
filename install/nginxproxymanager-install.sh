30f80c380ed81df1bfa76e505b38aff041bbbcf9
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2024-05-29 22:20:39 -0400

    Update nginxproxymanager-install.sh

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -64,7 +64,7 @@ $STD apt-get install -y nodejs
 msg_ok "Installed Node.js"
 
 msg_info "Installing pnpm"
-$STD npm install -g pnpm@8.15
+$STD npm install -g pnpm@9.1 #8.15 OLD VER
 msg_ok "Installed pnpm"
 
 RELEASE=$(curl -s https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest |
